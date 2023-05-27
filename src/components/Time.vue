6d03f5d021c145da9d8421331c091c8591616ebf
Jatin rana <engr.jatinrana@gmail.com>
2023-05-27 17:58:13 +0530

    Time and control componet is created and working properly

---
+++ src/components/Time.vue
@@ -0,0 +1,62 @@
+<template>
+    <section>
+    <div>Time</div>
+    <div>
+        <p id="time">{{ timer }}</p>
+    <Control title="Start" :control="start"/>
+    <Control title="Stop" :control="stop"/>
+    </div>
+    
+</section>
+</template>
+
+<script>
+import Control from './Control.vue'
+export default {
+    name:`Time`,
+
+    components:{
+        Control
+    },
+    data(){
+        return{
+           timer:0,
+           timerId:0
+
+        }
+        
+        
+    },
+    methods:{
+        // msg(){
+        //     alert("Button Call");
+        // },
+        start(){
+            this.timerId=setInterval(()=>{
+                this.timer++;
+            },1000);
+        },
+        stop(){
+            clearInterval(this.timerId);
+        }
+    }
+}
+</script>
+
+<style scoped>
+section{
+    background-color: aqua;
+    border: 2px solid;
+    width: 450px;
+    height: 385px;
+    border-radius: 50%;
+    /* display: flex; */
+    display: inline-block;
+    /* display: flex; */
+    align-items: center; 
+    
+}
+
+section div{
+}
+</style>
